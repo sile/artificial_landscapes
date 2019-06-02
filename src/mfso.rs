a4bb681e45dd565be42e37879f4a2cb4627d967f
Takeru Ohta <phjgt308@gmail.com>
2019-06-02 19:29:30 +0900

    Add `Hartmann` functions

--- src/mfso.rs
+++ src/mfso.rs
@@ -7,7 +7,7 @@ use crate::{Interval, Objective};
 use std::f64::consts::PI;
 use std::fmt;
 use std::iter;
-use std::num::NonZeroU64;
+use std::num::{NonZeroU64, NonZeroU8};
 
 const ZERO_TO_ONE: Interval = unsafe { Interval::new_unchecked(0.0, 1.0) };
 
@@ -243,5 +243,178 @@ impl Objective for Borehole {
 impl MultiFidelitySingleObjective for Borehole {
     fn max_cost(&self) -> Cost {
         self.cost_factor
+    }
+}
+
+/// Hartmann-3D function (multi fidelity).
+///
+/// See: [Multi-fidelity Gaussian Process Bandit Optimisation](https://arxiv.org/abs/1603.06288)
+#[derive(Debug, Clone)]
+pub struct Hartmann3d {
+    max_fidelity: NonZeroU8,
+    cost_factor: NonZeroU64,
+}
+impl Default for Hartmann3d {
+    fn default() -> Self {
+        Self::new(unsafe { NonZeroU8::new_unchecked(3) }, TEN)
+    }
+}
+impl Hartmann3d {
+    pub const fn new(max_fidelity: NonZeroU8, cost_factor: NonZeroU64) -> Self {
+        Self {
+            max_fidelity,
+            cost_factor,
+        }
+    }
+
+    fn alpha(&self, m: u8, i: usize) -> f64 {
+        const ALPHA: [f64; 4] = [1.0, 1.2, 3.0, 3.2];
+        const DELTA: [f64; 4] = [0.01, -0.01, -0.1, 0.1];
+
+        ALPHA[i] + (self.max_fidelity.get() - m) as f64 * DELTA[i]
+    }
+
+    fn f(&self, m: u8, xs: &[f64]) -> f64 {
+        const A: [[f64; 3]; 4] = [
+            [3.0, 10.0, 30.0],
+            [0.1, 10.0, 35.0],
+            [3.0, 10.0, 30.0],
+            [0.1, 10.0, 35.0],
+        ];
+        const P: [[f64; 3]; 4] = [
+            [0.3689, 0.1170, 0.2673],
+            [0.4699, 0.4387, 0.7470],
+            [0.1091, 0.8732, 0.5547],
+            [0.0381, 0.5743, 0.8828],
+        ];
+
+        (0..4)
+            .map(|i| {
+                let a = self.alpha(m, i);
+                let b = (0..3)
+                    .map(|j| A[i][j] * (xs[j] - P[i][j]).powi(2))
+                    .sum::<f64>();
+                a * (-b).exp()
+            })
+            .sum::<f64>()
+    }
+}
+impl Objective for Hartmann3d {
+    type Output = Outputs;
+
+    fn input_domain(&self) -> &[Interval] {
+        const DOMAIN: [Interval; 3] = [ZERO_TO_ONE, ZERO_TO_ONE, ZERO_TO_ONE];
+        &DOMAIN
+    }
+
+    fn evaluate(&self, xs: &[f64]) -> Self::Output {
+        let xs = Vec::from(xs);
+        let this = self.clone();
+        Outputs::new((0..self.max_fidelity.get()).map(move |m| {
+            let v = this.f(m + 1, &xs);
+            let c = unsafe { NonZeroU64::new_unchecked(this.cost_factor.get().pow(u32::from(m))) };
+            (c, v)
+        }))
+    }
+}
+impl MultiFidelitySingleObjective for Hartmann3d {
+    fn max_cost(&self) -> Cost {
+        unsafe {
+            NonZeroU64::new_unchecked(
+                self.cost_factor
+                    .get()
+                    .pow(u32::from(self.max_fidelity.get())),
+            )
+        }
+    }
+}
+
+/// Hartmann-6D function (multi fidelity).
+///
+/// See: [Multi-fidelity Gaussian Process Bandit Optimisation](https://arxiv.org/abs/1603.06288)
+#[derive(Debug, Clone)]
+pub struct Hartmann6d {
+    max_fidelity: NonZeroU8,
+    cost_factor: NonZeroU64,
+}
+impl Default for Hartmann6d {
+    fn default() -> Self {
+        Self::new(unsafe { NonZeroU8::new_unchecked(4) }, TEN)
+    }
+}
+impl Hartmann6d {
+    pub const fn new(max_fidelity: NonZeroU8, cost_factor: NonZeroU64) -> Self {
+        Self {
+            max_fidelity,
+            cost_factor,
+        }
+    }
+
+    fn alpha(&self, m: u8, i: usize) -> f64 {
+        const ALPHA: [f64; 4] = [1.0, 1.2, 3.0, 3.2];
+        const DELTA: [f64; 4] = [0.01, -0.01, -0.1, 0.1];
+
+        ALPHA[i] + (self.max_fidelity.get() - m) as f64 * DELTA[i]
+    }
+
+    fn f(&self, m: u8, xs: &[f64]) -> f64 {
+        const A: [[f64; 6]; 4] = [
+            [10.0, 3.0, 17.0, 3.5, 1.7, 8.0],
+            [0.05, 10.0, 17.0, 0.1, 8.0, 14.0],
+            [3.0, 3.5, 1.7, 10.0, 17.0, 8.0],
+            [17.0, 8.0, 0.05, 10.0, 0.1, 14.0],
+        ];
+        const P: [[f64; 6]; 4] = [
+            [0.1312, 0.1696, 0.5569, 0.0124, 0.8283, 0.5886],
+            [0.2329, 0.4135, 0.8307, 0.3736, 0.1004, 0.9991],
+            [0.2348, 0.1451, 0.3522, 0.2883, 0.3047, 0.6650],
+            [0.4047, 0.8828, 0.8732, 0.5743, 0.1091, 0.0381],
+        ];
+
+        (0..4)
+            .map(|i| {
+                let a = self.alpha(m, i);
+                let b = (0..6)
+                    .map(|j| A[i][j] * (xs[j] - P[i][j]).powi(2))
+                    .sum::<f64>();
+                a * (-b).exp()
+            })
+            .sum::<f64>()
+    }
+}
+impl Objective for Hartmann6d {
+    type Output = Outputs;
+
+    fn input_domain(&self) -> &[Interval] {
+        const DOMAIN: [Interval; 6] = [
+            ZERO_TO_ONE,
+            ZERO_TO_ONE,
+            ZERO_TO_ONE,
+            ZERO_TO_ONE,
+            ZERO_TO_ONE,
+            ZERO_TO_ONE,
+        ];
+        &DOMAIN
+    }
+
+    fn evaluate(&self, xs: &[f64]) -> Self::Output {
+        let xs = Vec::from(xs);
+        let this = self.clone();
+        Outputs::new((0..self.max_fidelity.get()).map(move |m| {
+            let v = this.f(m + 1, &xs);
+            let c = unsafe { NonZeroU64::new_unchecked(this.cost_factor.get().pow(u32::from(m))) };
+            (c, v)
+        }))
+    }
+}
+impl MultiFidelitySingleObjective for Hartmann6d {
+    fn max_cost(&self) -> Cost {
+        unsafe {
+            NonZeroU64::new_unchecked(
+                self.cost_factor
+                    .get()
+                    .pow(u32::from(self.max_fidelity.get())),
+            )
+        }
     }
 }
